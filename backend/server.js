e4bce460ad634d914f227e0a83ff37eae6f1c74b
arpita244 <arpitatiwari8756@gmail.com>
2025-01-20 23:01:42 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.post("/submit",(req,res)=>{
+    const { name, age, gender, hobbies, contact, message } = req.body;
+    console.log("Received form data: ",{name, age, gender, hobbies, contact, message});
+     // Create a hopeful message
+  const hopefulMessage = `Dear ${name},\n
+  At the age of ${age}, you're already creating a unique story. As a ${gender},
+   your passion for "${hobbies}" and your willingness to share your thoughts are 
+   truly inspiring. Keep shining and reaching out! The world is brighter because of 
+   people like you.`;
+
+  res.status(200).json({ hopefulMessage });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
